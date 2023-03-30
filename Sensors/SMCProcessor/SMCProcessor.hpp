41995a6177c8672cfe22cecf41c24ee9b39a5c39
Sept Ulchre <asepmscom@Septs-MacBook.local>
2023-03-30 18:31:18 +0700

    Fix stuck when turbo boost disable

--- Sensors/SMCProcessor/SMCProcessor.hpp
+++ Sensors/SMCProcessor/SMCProcessor.hpp
@@ -212,24 +212,6 @@ class EXPORT SMCProcessor : public IOService {
 	 *  CPU model info
 	 */
 	uint32_t cpuFamily {0}, cpuModel {0}, cpuStepping {0};
-		
-	/**
-	 *  Array of thread' handles used to update cpu specific counters
-	 */
-	thread_call_t threadHandles[CPUInfo::MaxCpus] {};
-	
-	/**
-	 *  Helper function to bind current thread to specified CPU
-	 */
-	bool bindCurrentThreadToCpu(uint32_t cpu);
-	
-	/**
-	 *  static Thread Entry method
-	 *
-	 *  @param param0 unused
-	 *  @param param1 unused
-	 */
-	static void staticThreadEntry(thread_call_param_t param0, thread_call_param_t param1);
 
 	/**
 	 *  Timer scheduling status
@@ -264,7 +246,7 @@ class EXPORT SMCProcessor : public IOService {
 	/**
 	 *  Refresh counter values callback
 	 */
-	void updateCounters(uint32_t cpu);
+	void updateCounters();
 
 	/**
 	 *  Refresh sensor state on timer basis
